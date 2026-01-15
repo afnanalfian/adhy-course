92c3dcd5e1685b6c6c732b46ab4a6ca66a482be3
afnanalfian <afnan.02alf@gmail.com>
2026-01-15 20:08:03 +0800

    perbaikan tentor

--- app/Http/Controllers/Course/CourseController.php
+++ app/Http/Controllers/Course/CourseController.php
@@ -15,16 +15,16 @@ class CourseController extends Controller
         $q = $request->query('q');
 
         $user = auth()->user();
-        $isTentor = $user ? $user->hasRole('tentor') : false;
+        $isTentor = $user->hasRole('tentor');
 
         $courses = Course::with('teachers.user')
             ->when($q, fn($qr) => $qr->where('name', 'like', "%{$q}%"))
-            // ->when($isTentor, function($query) use ($user) {
-            //     // Filter hanya course yang diajari oleh tentor ini
-            //     $query->whereHas('teachers', function($q) use ($user) {
-            //         $q->where('user_id', $user->id);
-            //     });
-            // })
+            ->when($isTentor, function($query) use ($user) {
+                // Filter hanya course yang diajari oleh tentor ini
+                $query->whereHas('teachers', function($q) use ($user) {
+                    $q->where('user_id', $user->id);
+                });
+            })
             ->orderBy('name')
             ->paginate(12)
             ->withQueryString();
