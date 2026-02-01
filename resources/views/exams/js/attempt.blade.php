ecdfa5b6d888e78bdfcf9e98ec026bd993abbe81
afnanalfian <afnan.02alf@gmail.com>
2026-02-01 14:32:49 +0800

    perbaikan halaman ujian

--- resources/views/exams/js/attempt.blade.php
+++ resources/views/exams/js/attempt.blade.php
@@ -419,4 +419,19 @@
             }
         });
     </script>
+    <script>
+    window.MathJax = {
+        tex: {
+            inlineMath: [['\\(', '\\)']]
+        }
+    };
+    </script>
+
+    <script src="https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js"></script>
+
+    <script>
+    document.addEventListener('DOMContentLoaded', () => {
+        MathJax.typesetPromise();
+    });
+    </script>
 @endpush
