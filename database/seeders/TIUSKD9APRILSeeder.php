12b2246c7170e922b030b895f703fbab72b37d9d
afnanalfian <afnan.02alf@gmail.com>
2026-04-02 02:29:15 +0800

    tiu part 2

--- database/seeders/TIUSKD9APRILSeeder.php
+++ database/seeders/TIUSKD9APRILSeeder.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 use Carbon\Carbon;
 
-class TIUSeederPart1 extends Seeder
+class TIUSKD9APRILSeeder extends Seeder
 {
     public function run()
     {
